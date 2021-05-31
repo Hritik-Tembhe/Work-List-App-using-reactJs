9cb84bfa221bb1b3b4cb1dff19089bdae5226904
Hritik-Tembhe <84648355+Hritik-Tembhe@users.noreply.github.com>
2021-05-31 19:59:49 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index7.css';
+
+import App15 from "./App15";
+ReactDOM.render(
+  <App15/>,
+  document.getElementById("root")
+);
